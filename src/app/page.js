849686a3faf8af89f73c8c4a417295fea5c218eb
Chad Augur <chad@fridayarts.com>
2023-09-30 12:59:43 -0700

    Setting up bablon

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { draftMode } from 'next/headers'
-import Post from '@/app/components/posts/post';
+import Event from '@/app/components/events/event';
 
 export default async function Home() {
     const { isEnabled } = draftMode();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Post />
+      <Event />
     </main>
   );
 }
