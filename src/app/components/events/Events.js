bee74ea243f601c35c942b98819db851e9e5e159
Chad Augur <chad@fridayarts.com>
2024-01-02 10:23:29 -0800

    Server Side destructuring issue

--- src/app/components/events/Events.js
+++ src/app/components/events/Events.js
@@ -25,6 +25,9 @@ export default async function Events() {
         upcoming: [],
         past: []
     });
+
+    if (!past.length) return;
+    
     // Sorts future events from oldest to newest, and takes the first one, this is the next event
     const [next, ...future] = upcoming.sort(({ dateAndTime: a }, { dateAndTime: b }) => {
         const aDate = new Date(a);
@@ -39,17 +42,18 @@ export default async function Events() {
     return (
         <>
         <section className="w-full overflow-hidden relative flex items-start lg:items-center justify-center h-auto min-h-screen mx-auto mt-[-150px] pt-[150px] md:pt-[185px] lg:pt-[150px] pb-8 px-8 lg:pb-24 lg:px-24 mb-16 light">
-            <Event {...hero} hero={true} timeLine="future"/>
+            {hero && <Event {...hero} hero={true} timeLine="future"/>}
         </section>
         {!!(future.length) &&
         <section className="container px-8 md:px-24 my-[4rem] flex flex-col gap-8">
             <h3 className="text-2xl text-left font-extrabold leading-tight">Future Events</h3>
-            {future.map((newEvent, i) => <Event key={i} { ...newEvent } timeLine="future" />)}
+            {future.map((newEvent = {}, i) => <Event key={i} { ...newEvent } timeLine="future" />)}
         </section>}
+        {!!(theRealPast.length) &&
         <section className="container px-8 md:px-24 flex flex-col gap-8">
             <h3 className="text-2xl text-left font-extrabold leading-tight">Past Events</h3>
-            {theRealPast.map((newEvent, i) => <Event key={i} { ...newEvent } />)}
-        </section>
+            {theRealPast.map((newEvent = {}, i) => <Event key={i} { ...newEvent } />)}
+        </section>}
         </>
     );
 }
