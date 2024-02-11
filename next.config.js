fd686633b0bf1a3db77f964463efac061b788260
Chad Augur <chad@fridayarts.com>
2024-02-11 11:16:56 -0800

    Current Stoping point for Babalon Salon

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack(config, { dev = false }) {
+        if (!dev) {
+            config.optimization.minimize = true;
+            // Additional customizations go here
+        }
+        return config;
+    },
     reactStrictMode: true,
     compiler: {
         styledComponents: true,
-    }
+    },
 }
 
 module.exports = nextConfig
