812342640e251bdfa79a155696cb54129899908b
Chad Augur <chad@fridayarts.com>
2024-04-01 16:31:35 -0700

    Tenple is not a thing

--- src/app/temple/page.js
+++ src/app/temple/page.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EventCollection from '@/components/events/EventCollection';
 
 export const metadata = {
-    title: 'Tenple of Thelema: Initiation for the New Aeon',
+    title: 'Temple of Thelema: Initiation for the New Aeon',
     description: 'Founded in 1989 by Phyllis Seckler, Anna-Kria King, and James A. Eshelman, Temple of Thelema is an innovative organiztion with the mission of raising adepts.',
     keywords: 'A∴A∴, Soror Estai, Jane Wolfe, Soror Meral, Phyllis Seckler, James A. Eshelman,, Initiaion, Cipher Manuscript, LA, NYC, Bay Area',
     metadataBase: new URL('https://thelema.org/tmeple'),
