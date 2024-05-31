1f5b04e06310749ee54373205d0965abc5d02e82
Chad Augur <chad@fridayarts.com>
2024-05-30 19:22:01 -0700

    Keyowrd

--- src/app/publications/page.js
+++ src/app/publications/page.js
@@ -9,7 +9,7 @@ import AbesBooksLogo from '@/vectors/AbesBooksLogo';
 export const metadata = {
     title: 'College of Thelema: Publications',
     description: 'Beginning with In the Continuum, College Of Thelema has a rich history of publishing high quality Thelemic writings spanning 50years.',
-    keywords: 'A∴A∴, Soror Meral, Phyllis Seckler, In the Continuum, The Mystical and Magical System of the A∴A∴, 776.5, Visions & Voices, Pearls Of Wisdom, Black Pearl, Liber VII',
+    keywords: 'A∴A∴, Soror Meral, Phyllis Seckler, James A. Eshleman, In the Continuum, The Mystical and Magical System of the A∴A∴, 776.5, Visions & Voices, Pearls Of Wisdom, Black Pearl, Liber VII',
     alternates: {
       canonical: '/publications'
     },
